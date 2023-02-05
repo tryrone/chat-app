41d8a69dab364f3495f45db6f6a8b48db97ab431
tryrone <tegararuvwe@gmail.com>
2023-02-05 20:51:30 +0100

    project setup

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+"use strict";
+exports.__esModule = true;
+var expo_status_bar_1 = require("expo-status-bar");
+var react_native_1 = require("react-native");
+function App() {
+    return (<react_native_1.View style={styles.container}>
+      <react_native_1.Text>Open up App.js to start working on your app!</react_native_1.Text>
+      <expo_status_bar_1.StatusBar style="auto"/>
+    </react_native_1.View>);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+exports["default"] = App;
+var styles = react_native_1.StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 });
